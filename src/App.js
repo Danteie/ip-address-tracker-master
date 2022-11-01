6d1a1a401af46a4ec9f10a321fe7b96a74703f7b
Danteie <aleksandar.djuratovic93@gmail.com>
2022-11-01 18:30:05 +0100

    adding header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
+import arrow from '../src/Assetes/icon-arrow.svg'
 
 function App() {
   return (
     <div className="App">
-      <div>TOP</div>
-      <div>Middle</div>
+      <div className='header'>
+        <h1>IP Address Tracker</h1>
+        <div>
+          <input type='text'/>
+          <button className='top-button'><img src={arrow}/></button>
+        </div>
+        
+      </div>
+
+    <div className='data'>
+      <div>
+        <div>IP ADDRESS</div>
+        <div>192.212.174.101</div>
+      </div>
+      <div>
+        <div>LOCATION</div>
+        <div>192.212.174.101</div> 
+      </div>
+      <div>
+        <div>TIMEZONES</div>
+        <div>192.212.174.101</div>
+      </div>
+      <div>
+        <div>ISP</div>
+        <div>192.212.174.101</div>
+       </div>
+    </div>
+      
       <div>MAP</div>
     </div>
   );
