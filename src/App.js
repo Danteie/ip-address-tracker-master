710e787640a8fd749f4a68e25065d86ac96f5307
Danteie <aleksandar.djuratovic93@gmail.com>
2022-11-08 15:04:16 +0100

    adding styling

--- src/App.js
+++ src/App.js
@@ -4,37 +4,52 @@ import { MapContainer, TileLayer, useMap,Marker,Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 
 function App() {
+
+
+
+  function textEdit(event){
+    console.log(event);
+  }
+
+
+
   return (
 
     <div className="App">
       <div className='header'>
         <h1>IP Address Tracker</h1>
         <div>
-          <input type='text'/>
-          <button className='top-button'><img src={arrow}/></button>
+          <input type='text' onChange={textEdit} placeholder='Search for any IP address or domain'/>
+          <button className='top-button' ><img src={arrow}/></button>
         </div>
-      </div>
 
-      <div className='data'>
-        <div>
+
+        <div className='data'>
+        <div className='vertical'>
           <div>IP ADDRESS</div>
           <div>192.212.174.101</div>
         </div>
-        <div>
+        <></>
+        <div className='vertical'>
           <div>LOCATION</div>
           <div>192.212.174.101</div> 
         </div>
-        <div>
+        <div className='vertical'>
           <div>TIMEZONES</div>
           <div>192.212.174.101</div>
         </div>
-        <div>
+        <div className='last'>
           <div>ISP</div>
           <div>192.212.174.101</div>
         </div>
       </div>
+      </div>
+
+
 
       <div className='maps'>
+        
+   
         <MapContainer center={[51.505, -0.09]} zoom={13} >
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
